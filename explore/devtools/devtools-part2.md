19e3d9a55e66ce81d18722f1f5df3242c6133ccd
longhodac-ucsd <lhodac@ucsd.edu>
2025-04-30 03:51:16 -0700

    final update

---
+++ explore/devtools/devtools-part2.md
@@ -0,0 +1,2 @@
+1. typeof result was "string" and not "number". Both num1 and num2 are strings.
+2. I would convert the input values to number before adding/
--- expose/javacript/test.js
+++ expose/javacript/test.js
@@ -1,8 +1,19 @@
-function printNums(){
-    console.log(1);
-    setTimeout(function() { console.log(2); }, 1000);
-    setTimeout(function() { console.log(3)}, 0);
-    console.log(4);
-}
+// part1.js
 
-printNums();
+function printSum() {
+    debugger
+    let num1 = Number(document.getElementById("num1").value);
+    let num2 = Number(document.getElementById("num2").value);
+    document.getElementById("sum").innerHTML = "Sum: " + calculateSum(num1, num2);
+  }
+  
+  function calculateSum(num1, num2) {
+    let result = num1 + num2
+    return result
+  }
+  
+  function init() {
+    document.getElementById('printSum').addEventListener('click', printSum);
+  }
+  
+  window.addEventListener('DOMContentLoaded', init);
